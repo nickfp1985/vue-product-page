949aa891bf2b41c8928b3458b737e2729e27b9b1
Nick Peddie <nfpeddie@gmail.com>
2020-01-02 10:50:50 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    brand: 'Vue Mastery',
+    image: './assets/vmSocks-green.jpg',
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    }
+  }
+})

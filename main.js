1a208879a74b1849be40644710e29655a1535a69
Nick Peddie <nfpeddie@gmail.com>
2020-01-13 12:18:02 -0800

    created basic form validation for the product reviews

--- main.js
+++ main.js
@@ -115,6 +115,14 @@ Vue.component('product', {
 Vue.component('product-review', {
   template: `
     <form class="review-form" @submit.prevent="onSubmit">
+
+      <p v-if"errors.length">
+        <b>Please, correct these error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+      
       <p>
         <label for="name">Name:</label>
         <input id="name" v-model="name">
@@ -141,20 +149,28 @@ Vue.component('product-review', {
     return {
       name: null,
       review: null,
-      rating: null
+      rating: null,
+      errors: []
     }
   },
   methods: {
     onSubmit() {
-      let productReview = {
-        name: this.name,
-        review: this.review,
-        rating: this.rating
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
       }
-      this.$emit('review-submitted', productReview)
-      this.name = null
-      this.review = null
-      this.rating = null
+      else {
+        if(!this.name) this.errors.push("Name required.")
+        if(!this.review) this.errors.push("Review required.")
+        if(!this.rating) this.errors.push("Rating required.")
+      }
     }
   }
 })
